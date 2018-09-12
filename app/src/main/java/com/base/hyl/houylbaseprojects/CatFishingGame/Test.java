b315a1256e98b30c3084f2a08d3ec760cdc7fa42
itw_houyl <itw_houyl234>
2018-09-12 17:17:14 +0800

    整理一下 内容

---
+++ app/src/main/java/com/base/hyl/houylbaseprojects/CatFishingGame/Test.java
@@ -0,0 +1,10 @@
+package com.base.hyl.houylbaseprojects.CatFishingGame;
+
+import java.util.Stack;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ app/src/main/java/com/base/hyl/houylbaseprojects/Main2Activity.java
@@ -0,0 +1,14 @@
+package com.base.hyl.houylbaseprojects;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Main2Activity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        setFinishOnTouchOutside(false);//点击空白处，窗体不关闭
+    }
+}
