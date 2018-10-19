3937cb8f124d39069c9928a6d787eb1fb6e66c83
itw_houyl <itw_houyl234>
2018-10-19 15:16:10 +0800

    测试 中

--- app/src/main/java/com/base/hyl/houylbaseprojects/MainActivity.java
+++ app/src/main/java/com/base/hyl/houylbaseprojects/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
 
+import com.timmy.tdialog.TDialog;
+
 public class MainActivity extends Activity {
 
     @Override
@@ -20,5 +22,6 @@ public class MainActivity extends Activity {
             startActivity(new Intent(MainActivity.this,Main2Activity.class));
             }
         });
+        TDialog.
     }
 }
---
+++ app/src/main/java/com/base/hyl/houylbaseprojects/fgDialog/listener/OnBindViewListener.java
@@ -0,0 +1,8 @@
+package com.base.hyl.houylbaseprojects.fgDialog.listener;
+
+
+import com.base.hyl.houylbaseprojects.fgDialog.base.BindViewHolder;
+
+public interface OnBindViewListener {
+    void bindView(BindViewHolder viewHolder);
+}
