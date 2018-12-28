79c181071fb0c4b2356b5b979ebd06049fe719b0
itw_houyl <itw_houyl234>
2018-12-28 10:44:29 +0800

    添加输入 工具

---
+++ houbasemodule/src/main/java/com/base/hyl/houbasemodule/utils/InputTools.java
@@ -0,0 +1,77 @@
+package com.base.hyl.houbasemodule.utils;
+
+import android.app.Activity;
+import android.content.Context;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class InputTools {
+
+    //隐藏虚拟键盘
+    public static void HideKeyboard(Activity activity) {
+        if (null == activity) {
+            return;
+        }
+        if (null != activity.getCurrentFocus() && null != activity.getCurrentFocus().getWindowToken()) {
+
+            InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+
+            imm.hideSoftInputFromWindow(activity.getCurrentFocus().getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+        }
+    }
+
+    //显示虚拟键盘
+    public static void ShowKeyboard(View v) {
+        InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+
+        imm.showSoftInput(v, InputMethodManager.SHOW_FORCED);
+
+    }
+
+    //强制显示或者关闭系统键盘
+    public static void KeyBoard(final EditText txtSearchKey, final String status) {
+
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                InputMethodManager m = (InputMethodManager)
+                        txtSearchKey.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (status.equals("open")) {
+                    m.showSoftInput(txtSearchKey, InputMethodManager.SHOW_FORCED);
+                } else {
+                    m.hideSoftInputFromWindow(txtSearchKey.getWindowToken(), 0);
+                }
+            }
+        }, 300);
+    }
+
+    //通过定时器强制隐藏虚拟键盘
+    public static void TimerHideKeyboard(final View v) {
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+                if (imm.isActive()) {
+                    imm.hideSoftInputFromWindow(v.getApplicationWindowToken(), 0);
+                }
+            }
+        }, 10);
+    }
+
+    //输入法是否显示着
+    public static boolean KeyBoard(EditText edittext) {
+        boolean bool = false;
+        InputMethodManager imm = (InputMethodManager) edittext.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        if (imm.isActive()) {
+            bool = true;
+        }
+        return bool;
+
+    }
+}
