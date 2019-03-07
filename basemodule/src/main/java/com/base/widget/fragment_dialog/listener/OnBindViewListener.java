36cb6aab2b4c66edccbdcb5ed9a61159df8d0744
itw_houyl <itw_houyl234>
2019-03-07 10:58:42 +0800

    TDialog 处理 代码提交

---
+++ basemodule/src/main/java/com/base/widget/fragment_dialog/listener/OnBindViewListener.java
@@ -0,0 +1,8 @@
+package com.base.widget.fragment_dialog.listener;
+
+
+import com.base.widget.fragment_dialog.base.BindViewHolder;
+
+public interface OnBindViewListener {
+    void bindView(BindViewHolder viewHolder);
+}
---
+++ basemodule/src/main/java/com/base/widget/fragment_dialog/listener/OnViewClickListener.java
@@ -0,0 +1,11 @@
+package com.base.widget.fragment_dialog.listener;
+
+import android.view.View;
+
+import com.base.widget.fragment_dialog.TDialog;
+import com.base.widget.fragment_dialog.base.BindViewHolder;
+
+
+public interface OnViewClickListener {
+    void onViewClick(BindViewHolder viewHolder, View view, TDialog tDialog);
+}
