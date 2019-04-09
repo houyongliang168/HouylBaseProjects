5728a34c86a36b34f40586bb09e9f1607faee97b
itw_houyl <itw_houyl234>
2019-04-09 14:16:42 +0800

    测试 activity 与 fragment 发现 butterknife 在使用的时候，会报空指针异常，view 对象放在 Fragment 中请求后展示，疑惑onresume 之后Fragment 还会直接走 onCreate ?测试 发现 Fragment 必在onCreate 之后进行 onResume 的操作
    所有 考虑 butterknife 在oncreateview 中没有初始化操作
    不是必现
    代码提交

--- app/src/main/java/com/base/hyl/houylbaseprojects/Main3Activity.java
+++ app/src/main/java/com/base/hyl/houylbaseprojects/Main3Activity.java
@@ -1,24 +1,36 @@
 package com.base.hyl.houylbaseprojects;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.FrameLayout;
 
 import com.base.common.log.MyToast;
 import com.orhanobut.logger.Logger;
 
-public class Main3Activity extends AppCompatActivity {
+
+public class Main3Activity extends AppCompatActivity implements Test1Fragment.OnFragmentInteractionListener {
     private static String TAG = Main3Activity.class.getSimpleName();
+    
+    
+    private Test1Fragment fg1;
+    private FrameLayout fl;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        fl = (FrameLayout) findViewById(R.id.fl);
         setSupportActionBar(toolbar);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -48,6 +60,36 @@ public class Main3Activity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         Logger.e(TAG + "onResume");
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                fg1=  new Test1Fragment();
+                startFragment(fg1);
+            }
+        }, 2000);
+//        fg1=  new Test1Fragment();
+//        startFragment(fg1);
+//        TimerTask task = new TimerTask() {
+//            @Override
+//            public void run() {
+//                f1=  new Fragment();
+//            }
+//        };
+//        Timer timer = new Timer();
+//        timer.schedule(task, 2000);
+
+        // 通过静态方法创建ScheduledExecutorService的实例
+//         ScheduledExecutorService mScheduledExecutorService = Executors.newScheduledThreadPool(4);
+
+        // 延时任务
+//        mScheduledExecutorService.schedule(threadFactory.newThread(new Runnable() {
+//            @Override
+//            public void run() {
+//                Log.e("lzp", "first task");
+//            }
+//        }), 1, TimeUnit.SECONDS);
+
+
     }
 
     @Override
@@ -60,5 +102,18 @@ public class Main3Activity extends AppCompatActivity {
     protected void onRestart() {
         super.onRestart();
         Logger.e(TAG + "onRestart");
+    }
+
+   public void  startFragment(Test1Fragment t1){
+       android.support.v4.app.FragmentManager supportFragmentManager = getSupportFragmentManager();
+       android.support.v4.app.FragmentTransaction fragmentTransaction = supportFragmentManager.beginTransaction();
+       fragmentTransaction.replace(R.id.fl, t1);
+       fragmentTransaction.commit();
+        
+   }
+
+    @Override
+    public void onFragmentInteraction(Uri uri) {
+        
     }
 }
